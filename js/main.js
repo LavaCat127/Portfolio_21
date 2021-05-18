707dbea0d5d413a3ff37ade64e9d982fbd2abda1
LavaCat127 <46032950+LavaCat127@users.noreply.github.com>
2021-05-17 23:17:05 -0700

    Duplicate
    Duplicate

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$(".openmenu").click(function(e){
+  $( ".mobileMenu" ).addClass( "active" );
+  e.preventDefault();
+});
+
+$(".closemenu").click(function(a){
+  $( ".mobileMenu" ).removeClass( "active" );
+  a.preventDefault();
+});
+
+$(".arrowWork").click(function(){
+  $('html, body').animate({
+      scrollTop: $("#work").offset().top
+   }, 800);
+});
+
+$('.carousel').carousel();
